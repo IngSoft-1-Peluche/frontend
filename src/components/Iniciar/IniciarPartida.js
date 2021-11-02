e6c8bffebccbc89ce43481fb0ebb0fdcc86dcded
Santiago Linares <santilinares@hotmail.es>
2021-11-02 16:31:49 -0300

    Añade test para iniciar partida y agrega funcionalidad a iniciar partida

--- src/components/Iniciar/IniciarPartida.js
+++ src/components/Iniciar/IniciarPartida.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
 
 const IniciarPartida = () => {
 
   const {id,id_J}=useParams();
 
+  const fillData = (result) => {
+    let ele = document.getElementById('container');
+    ele.innerHTML = result;
+}
+
   const iniciar = async () => {
-    const data = await fetch(`http://localhost:8000/partidas/${id}?id_jugador=${id_J}` ,{
+      const data = await fetch(`http://localhost:8000/partidas/${id}?id_jugador=${id_J}` ,{
       method : 'PATCH',
       body: JSON.stringify({
       id_jugador:parseInt(id_J),
       id_partida:parseInt(id)
       }) 
     })
+    let resultado = await data.json()
+    console.log(resultado)
+    resultado = resultado["detail"]
+    if (resultado == undefined){
+      resultado = "Partida creada"
+    }
+    fillData(resultado)
   }
 
   return (
@@ -20,7 +32,13 @@ const IniciarPartida = () => {
       <button onClick={iniciar}>
         Iniciar partida
       </button>
+      <div id='container'>
+    	  <p>
+        	
+        </p>
+      </div>
     </div>
+    
   );
 }
 

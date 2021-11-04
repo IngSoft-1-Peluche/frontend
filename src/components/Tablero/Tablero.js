a58b4aef2dde505a040baa5452ce8aee207449a9
Exequiel <exeferreira7@mi.unc.edu.ar>
2021-11-04 15:42:36 -0300

    imagen tablero

---
+++ src/components/Tablero/Tablero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Tablero.css';
+import MisterioBoard from '../../assets/imagenes/MisterioBoard.jpeg'
+
+const Tablero = () =>{
+
+    return(
+        <img src={MisterioBoard}></img>
+    )
+
+}
+
+export default Tablero;

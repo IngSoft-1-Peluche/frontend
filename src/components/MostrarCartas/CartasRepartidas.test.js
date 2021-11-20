6e9198d28ff0357ea60b716b98b53c0a4ff64171
Santiago Linares <santilinares@hotmail.es>
2021-11-19 22:26:42 -0300

    Cartas repartidas completo

---
+++ src/components/MostrarCartas/CartasRepartidas.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render,screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
+import CartasRepartidas from './CartasRepartidas';
+import {jest} from '@jest/globals'
+
+
+test('Modulo renderiza correctamente', async () => {
+    Storage.prototype.getItem = jest.fn(() =>{ 
+        return '{"logueado":true}'
+    });
+    const ws = "" 
+    const cartas = ["Biblioteca", "Conde", "Alcoba", "Panteon"]
+    const component = render( <CartasRepartidas ws = {ws} cartas={cartas} />)
+    await component.findAllByText('Biblioteca')
+    await component.findAllByText('Conde')
+    await component.findAllByText('Alcoba')
+    await component.findAllByText('Panteon')
+})
+
